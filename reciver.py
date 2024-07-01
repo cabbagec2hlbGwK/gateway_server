d640cf35ef851e51715ccb0a759d8839ce8579a7
asher james <cabbageshell@protonmail.com>
2024-07-01 13:34:19 -0400

    working on the sending part of the smtp

--- reciver.py
+++ reciver.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import ssl
 import subprocess
+import smtplib
 from aiosmtpd.smtp import SMTP
 from aiosmtpd.controller import Controller
 from aiosmtpd.handlers import Debugging
@@ -24,19 +25,18 @@ context.load_cert_chain(f'cert{os.path.sep}cert.pem', f'cert{os.path.sep}key.pem
 #------------------------------------------------------------------------------
 
 class MessageHandler:
-    async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
-        if not address.endswith('@example.com'):
-            return '550 not relaying to that domain'
-        envelope.rcpt_tos.append(address)
-        return '250 OK'
-
     async def handle_DATA(self, server, session, envelope):
-        print(f'Message from {envelope.mail_from}')
-        print(f'Message for {envelope.rcpt_tos}')
-        print(f'Message data:\n{envelope.content.decode("utf8", errors="replace")}')
-        print('End of message')
-        return '250 Message accepted for delivery'
-
+        peer = session.peer
+        mailfrom = envelope.mail_from
+        rcpttos = envelope.rcpt_tos
+        message = message_from_bytes(envelope.content, policy=default)
+        #HERE MAYBE WOULD BE SAFER TO WALK CONTENTS AND PARSE/MODIFY ONLY MAIL BODY, BUT NO SIDE EFFECTS UNTIL NOW WITH MIME, ATTACHMENTS...
+        messagetostring = message.as_string() ### smtplib.sendmail WANTED BYTES or STRING, NOT email OBJECT.
+        print(messagetostring)
+        server = smtplib.SMTP('smtp.gmail.com', 25)
+        server.send_message(mailfrom, rcpttos, messagetostring) ### NEEDED TO INVERT ARGS ORDER
+        server.quit()
+        return '250 OK' ### ADDED RETURN
 
 # Pass SSL context to aiosmtpd
 class ControllerStarttls(Controller):

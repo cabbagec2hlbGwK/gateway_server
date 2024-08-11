96eabbc2f24f59c0218998c913a0e7967ba32266
asher james <cabbageshell@protonmail.com>
2024-08-11 17:40:14 -0400

    debug

--- worker.py
+++ worker.py
@@ -64,6 +64,7 @@ def main():
 
     @consumer.process
     def handel_event(message):
+        print("thie is s sjldifkndsoifnsoifniodsnfoidns fkndsoifnsdofnino")
         objKey = message.get("s3Key")
         data = s3Manager.s3Get(objKey)
         process(data.get("envelope","there was nothing ther"),args)

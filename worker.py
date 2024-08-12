4be1a67d89f16fbcc4d24e2a6f1436b79e73a0d6
asher james <cabbageshell@protonmail.com>
2024-08-12 00:28:58 -0400

    bug

--- worker.py
+++ worker.py
@@ -32,9 +32,10 @@ def process(envelope, args):
         metadata = attachment.get("Content-Type").split(";")
         contentType = metadata[0].strip()
         name = metadata[1].split("=")[1].replace('"','').strip()
-        print("inside the damn thing")
-        print(attachment.get_payload())
-        files =  {'test': (name, base64.b64decode(attachment.get_payload()), contentType)}
+        rawBits = attachment.get_payload()
+        if len(rawBits) % 4 == 0:
+            rawBits = base64.b64decode(attachment.get_payload())
+        files =  {'test': (name, rawBits, contentType)}
         url = f"http://{args.api}:5000/extract"
         res = requests.post(url, files=files)
         text += res.text

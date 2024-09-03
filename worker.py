0344231aadde1c7f647900cfd81d9932069fc425
asher james <cabbageshell@protonmail.com>
2024-09-03 15:04:34 -0400

    change in the worker module

--- worker.py
+++ worker.py
@@ -67,6 +67,7 @@ def process(envelope, args, message):
         if not url:
             raise Exception("AAPROVALSQS not found")
         producer = SqsProcucer(url)
+        print(f"this is the value :{message}")
         producer.send_message(json.dumps({"messageId":str(uuid.uuid4()),"pii":json.dumps(jres), "s3Key":message.get("s3Key"),"timeStamp":time.time()}))
         return 1
 

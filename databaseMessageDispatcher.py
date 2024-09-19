be926cf6ed90aac8f9ca5572c7591f96a61f6882
asher james <cabbageshell@protonmail.com>
2024-09-19 15:54:00 -0400

    deploying the email dispatcher to the dev

--- databaseMessageDispatcher.py
+++ databaseMessageDispatcher.py
@@ -54,6 +54,7 @@ def main():
     rdsSec = json.loads(get_secret(secretName))
     db = DcDatabase(secret=rdsSec, endpoint=rdsEndpoint, dbName="test1",firebaseConnector=None, dbTableName=tableN)
     getUpdate(db, pro)
+    db.connection.commit()
     
 
 
